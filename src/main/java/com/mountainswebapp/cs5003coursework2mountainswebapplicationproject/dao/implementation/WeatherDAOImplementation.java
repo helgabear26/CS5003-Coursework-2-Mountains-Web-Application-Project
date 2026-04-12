3306b2e00f978c7e4dc1a0b4d887b964f0ec3861
helgabear26 <helgabearvvpc@gmail.com>
2026-04-12 09:25:10 +0100

    DAOs and Beans and Entities are finished

--- src/main/java/com/mountainswebapp/cs5003coursework2mountainswebapplicationproject/dao/implementation/WeatherDAOImplementation.java
+++ src/main/java/com/mountainswebapp/cs5003coursework2mountainswebapplicationproject/dao/implementation/WeatherDAOImplementation.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class WeatherDAOImplementation implements WeatherDAO {
 
-    private static final String API_KEY = "d042061a3add667433b76449f3935c20";
+    private static final String API_KEY = ""
     private static final String API_URL = "";
 
     @Override
